15912ed71e33222e0adc980af983ec0ed6bb526f
elenacano <elenita.canocas@gmail.com>
2020-04-30 01:15:33 +0200

    Nuevas notificaciones

--- src/vista/VerNotificaciones.java
+++ src/vista/VerNotificaciones.java
@@ -8,7 +8,10 @@ import modelo.Notificacion;
 
 public class VerNotificaciones extends JPanel{
 	
-	private JLabel noticiulabel;
+	private JLabel mensaje = new JLabel("");
+	private JButton IrProyecto;
+	private JButton AprobarRegistro;
+	private JButton AprobarProyecto;
 	
 	public VerNotificaciones(){
 		BoxLayout notiLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
@@ -19,10 +22,37 @@ public class VerNotificaciones extends JPanel{
 		
     }
     
-	public void NotificacionesCiudadanoProyecto(Notificacion texto) {
-			final JPanel noticiup = new JPanel();
-			BoxLayout layoutNoticiup = new BoxLayout(noticiup, BoxLayout.Y_AXIS);
-			noticiup.setLayout(layoutNoticiup);
-			noticiulabel = new JLabel(texto.getTextoNotificacion());
+	public JPanel NotificacionCiudadanoProyecto() {
+		final JPanel notiCiudadano = new JPanel();
+		BoxLayout layoutNoticiu = new BoxLayout(notiCiudadano, BoxLayout.Y_AXIS);
+		notiCiudadano.setLayout(layoutNoticiu);
+		this.add(mensaje);
+		IrProyecto = new JButton("Ir al proyecto");
+		IrProyecto.setActionCommand("botonIrProyecto");
+		return notiCiudadano;
 		}
+	
+	public JPanel NotificacionAprobarRegistro() {
+		final JPanel notiRegistro = new JPanel();
+		BoxLayout layoutNotireg = new BoxLayout(notiRegistro, BoxLayout.Y_AXIS);
+		notiRegistro.setLayout(layoutNotireg);
+		this.add(mensaje);
+		AprobarRegistro = new JButton("Aprobar registro");
+		AprobarRegistro.setActionCommand("botonAprobarRegistro");
+		return notiRegistro;
+	}
+	
+	public JPanel NotificacionAprobarProyecto() {
+		final JPanel notiProye = new JPanel();
+		BoxLayout layoutNotiproye = new BoxLayout(notiProye, BoxLayout.Y_AXIS);
+		notiProye.setLayout(layoutNotiproye);
+		this.add(mensaje);
+		AprobarProyecto = new JButton("Aprobar proyecto");
+		AprobarProyecto.setActionCommand("botonAprobarProyecto");
+		return notiProye;
+	}
+	
+	public void setMensaje(String texto) {
+		mensaje.setText(texto);
+	}
 }
