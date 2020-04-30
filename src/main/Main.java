fbbc0b32737abf936551f0fecfa3114ee7aedd9c
pabloizaguirre <pablo@izaguirre.eu>
2020-04-30 23:21:22 +0200

    Cambiado el look and feel

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,18 @@ package main;
 
 import modelo.*;
 import vista.*;
+
+import javax.swing.UIManager;
+
 import controlador.*;
 
 public class Main {
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Exception e){
+			System.out.println("error");
+		}
 		Ventana frame = new Ventana();
 		Controlador controlador = new Controlador(frame);
 		frame.setControlador(controlador);
